94a02f397888d50ba96b1f5945073efaf7a1b285
nathy-mesquita <nnathaly@gmail.com>
2019-05-23 10:50:14 -0300

    Adicionando lista 6

--- Lista 5.cpp
+++ Lista 5.cpp
@@ -4,6 +4,7 @@ Ao digitar 1 exiba uma função do primeiro grau
 Ao digitar 2 exiba uma função do segundo grau
 
 Ao digitar 3 exiba uma função da sua escolha*/
+
 #include <cmath>
 #include <glut.h>
 
